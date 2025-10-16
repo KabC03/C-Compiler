9686be0d07a7808a14dbcab89bf5490a7e2deb91
JC <167163594+KabC03@users.noreply.github.com>
2025-10-17 03:44:00 +1100

    Implement calls in hashmap.h

--- src/Hashmap.h
+++ src/Hashmap.h
@@ -22,4 +22,14 @@ typedef struct Hashmap {
 } Hashmap;
 
 
+void hashmap_init(Hashmap *hashmap, size_t keySize, size_t valueSize);
+void hashmap_destroy(Hashmap *hashmap);
+bool hashmap_insert(Hashmap *hashmap, void *key, void *value);
+bool hashmap_delete(Hashmap *hashmap, void *key);
+const void *hashmap_find(Hashmap *hashmap, void *key);
+bool hashmap_set(Hashmap *hashmap, void *key, void *newValue);
+
+
+
+
 #endif // HASHMAP_H
