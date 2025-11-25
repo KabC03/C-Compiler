db9c0cd3d857b0b6c7184e61a0ff24c85050f7a1
Kabir <kcha0145@student.monash.edu>
2025-11-25 15:43:38 +1100

    - Rewrite tokeniser logic

--- main/main.c
+++ main/main.c
@@ -5,7 +5,7 @@
 int main(void) {
     tokenise_generate_token_set();   
 
-    char *str = "abc= +++";
+    char *str = "1111111111";
     Token token = tokenise_consume(&str);
     printf("%d, '%s'\n", token.id, token.str);
     token = tokenise_consume(&str);
